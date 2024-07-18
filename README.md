4b52c45f565bed00ce3d4aff8c48e536c12704fe
danialbagheri <bagheri.danial@gmail.com>
2024-07-18 12:43:33 +0100

    Updating the signature banner.

---
+++ README.md
@@ -0,0 +1,6 @@
+# A Javascript React signature generator
+
+### Deployment
+
+It's a simple react app. Use `npm run build` to build the project.
+The built Code (located at `build/*`)of this project need to be moved to the server to be served as a plain HTML using either `scp` or `rsync`.
--- signature/src/components/generator/template/Banner.js
+++ signature/src/components/generator/template/Banner.js
@@ -6,7 +6,7 @@ export default function Banner() {
       <img
         data-class="external"
         alt="Linco Care"
-        src="https://lincocare.s3.eu-west-2.amazonaws.com/lincocare-signature/banner2022.png"
+        src="https://lincocare.s3.eu-west-2.amazonaws.com/lincocare-signature/banner2024.png"
       />
     </a>
   );
